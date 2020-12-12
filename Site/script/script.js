04237165ec4d499a872e16d481e75729e1d0e1fc
Torre Baert <48511214+BaertTorre@users.noreply.github.com>
2020-12-12 22:19:11 +0100

    javascript quotes en select done

--- Site/script/script.js
+++ Site/script/script.js
@@ -1,24 +1,60 @@
 {
-let showResult = data => {
-    const Doc_quote = document.querySelector("#randomQuote");
-    let quoteText = data.value;
-    Doc_quote.innerHTML = quoteText;
-}
+	let docCategories
+	let docQuote
+	let docButton
 
+	function capitalizeFirstLetter(string) {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	}
 
-let getRandomQuote = async () => {
-	const url = `https://api.chucknorris.io/jokes/random`;
+	const showQuote = data => {
+		let htmlQuoteText = data.value
+		docQuote.innerHTML = htmlQuoteText;
+	}
 
-	// Met de fetch API proberen we de data op te halen.
-	const request = await fetch(url);
-	const data = await request.json();
-	console.log(data);
+	const showSelect = data => {
+		let htmlSelectText
+		for (let selectItem of data) {
+			htmlSelectText += `<option value="${selectItem}">${capitalizeFirstLetter(selectItem)}</option>`;
+		}
+		docCategories.innerHTML += htmlSelectText;
+	}
 
-	showResult(data);
-};
+	const getRandomQuote = async () => {
+		let selectedCategory = docCategories.value;
+		let url
+		if (selectedCategory == "everything") {
+			url = `https://api.chucknorris.io/jokes/random`;
+		}
+		else {
+			url = `https://api.chucknorris.io/jokes/random?category=${selectedCategory}`;
+		}
 
+		// Met de fetch API proberen we de data op te halen.
+		const request = await fetch(url);
+		const data = await request.json();
+		showQuote(data);
+	};
 
-document.addEventListener('DOMContentLoaded', function() {
-	//getRandomQuote();
-});
+	const getCategories = async () => {
+		const url = `https://api.chucknorris.io/jokes/categories`;
+
+		// Met de fetch API proberen we de data op te halen.
+		const request = await fetch(url);
+		const data = await request.json();
+		showSelect(data);
+	};
+
+	document.addEventListener('DOMContentLoaded', function () {
+		docCategories = document.querySelector("#categories");
+		docQuote = document.querySelector("#quote-field");
+		docButton = document.querySelector("#buttonRefresh");
+
+		docButton.addEventListener("click", function (event) {
+			getRandomQuote();
+		});
+
+		getCategories();
+		getRandomQuote();
+	});
 }
