90a8add455d66e3a8e9e5aef68dcce8eafd5b3e3
Torre Baert <48511214+BaertTorre@users.noreply.github.com>
2020-12-16 19:33:10 +0100

    complete repository

---
+++ Chuck Norris likes api/Models/LikesEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chuck_Norris_likes_api.Models
+{
+    class LikesEntity : TableEntity
+    {
+        public LikesEntity()         // anders werkt await cloudTable.ExecuteQuerySegmentedAsync<RegistrationEntity>(rangeQuery, null); niet
+        {
+
+        }
+
+        public LikesEntity(string id, string category)
+        {
+            //Can be changed to whatevery you want
+            PartitionKey = category;
+            RowKey = id; // Has to be unique for each row you add
+
+        }
+
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public string Id { get; set; }
+    }
+}
